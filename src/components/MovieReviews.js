3831008c4342bf5cc7aca29ff275e14f0b6932a8
Randall Robinson <keith_adams007@yahoo.com>
2018-07-30 17:01:17 +0000

    automatically backed up by learn

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,18 @@
 // Code MovieReviews Here
+import React from 'react'
+
+
+class MovieReviews extends React.Component {
+
+  render() {
+    return (
+    <div class="review-list">
+    </div>
+    )
+  }
+  
+}
+
+
+
+export default MovieReviews
